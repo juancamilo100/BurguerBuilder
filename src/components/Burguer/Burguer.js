84e2318f1cd1885d86ff30c51ea74a6ea34b326f
JuanEspinosa <juancamilo100@hotmail.com>
2018-12-01 16:30:52 -0500

    Refactoring burguer builder component

--- src/components/Burguer/Burguer.js
+++ src/components/Burguer/Burguer.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import classes from './Burguer.module.css'
+import BurguerIngredient from './BurguerIngredient/BurguerIngredient'
 
 const Burguer = props => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map((ingredientKey) => {
+            return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
+                return <BurguerIngredient key={ingredientKey + index} type={ingredientKey} />;
+            });
+        });
+
+    return (
+        <div className={classes.Burguer}>
+            <BurguerIngredient type="bread-top" />
+            <BurguerIngredient type="cheese" />
+            <BurguerIngredient type="meat" />
+            <BurguerIngredient type="bread-bottom" />
+        </div>
+    )
 }
+
+Burguer.propTypes = {}
 
 export default Burguer
--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -1,14 +1,24 @@
 import React, { Component, Fragment } from 'react'
+import moduleName from '../../components/Burguer/Burguer'
+import Burguer from '../../components/Burguer/Burguer'
 
 export default class BurguerBuilder extends Component {
-  render() {
-    return (
-      <Fragment>
-        <div>Burguer</div>
-        <div>Build Controls</div>
-      </Fragment>
-    )
-  }
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        },
+
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Burguer ingredients={this.state.ingredients}/>
+                <div>Build Controls</div>
+            </Fragment>
+        )
+    }
 }
-
-
