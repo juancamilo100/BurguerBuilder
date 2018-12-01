d30f726d69ab65ddfc2c49f152e94ff003b5fa0e
JuanEspinosa <juancamilo100@hotmail.com>
2018-12-01 15:43:37 -0500

    Creating ingredients

---
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react'
+
+export default class BurguerBuilder extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>Burguer</div>
+        <div>Build Controls</div>
+      </Fragment>
+    )
+  }
+}
+
+
