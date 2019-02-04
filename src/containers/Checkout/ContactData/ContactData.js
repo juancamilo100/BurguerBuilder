63ee84d39f868fa9179c3a6ae916e1e49045c921
JuanEspinosa <juancamilo100@hotmail.com>
2019-02-03 21:36:35 -0500

    Added form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -9,11 +9,11 @@ import Input from '../../../components/UI/Input/Input';
 class ContactData extends Component {
     state = {
         orderForm: {
-            name: this.IFormElement('input', { type: 'text', placeholder: 'Your Name' }),
-            street: this.IFormElement('input', { type: 'text', placeholder: 'Street' }),
-            zipCode: this.IFormElement('input', { type: 'text', placeholder: 'ZIP CODE' }),
-            country: this.IFormElement('input', { type: 'text', placeholder: 'Country' }),
-            email: this.IFormElement('input', { type: 'email', placeholder: 'Your email' }),
+            name: this.IFormElement('input', { type: 'text', placeholder: 'Your Name' }, { required: true, minLength: 3 }),
+            street: this.IFormElement('input', { type: 'text', placeholder: 'Street' }, { required: true, minLength: 3 }),
+            zipCode: this.IFormElement('input', { type: 'text', placeholder: 'ZIP CODE' }, { required: true, minLength: 5 }),
+            country: this.IFormElement('input', { type: 'text', placeholder: 'Country' }, { required: true, minLength: 5 }),
+            email: this.IFormElement('input', { type: 'email', placeholder: 'Your email' }, { required: true, minLength: 5 }),
             deliveryMethod: this.IFormElement('select', { options: [
                 {value: 'fastest', displayValue: 'Fastest'},
                 {value: 'cheapest', displayValue: 'Cheapest'},
@@ -22,26 +22,50 @@ class ContactData extends Component {
         loading: false
     }
 
-    IFormElement(type, config) {
+    IFormElement(type, config, validation) {
         return {
             elementType: type,
             elementConfig: config,
-            value: ''
+            value: '',
+            validation: validation,
+            valid: false,
+            touched: false
         }
+    }
+
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if(rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if(rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        return isValid;
     }
 
     orderHandler = (event) => {
         event.preventDefault();
 
-        const order = {
-            ingredients: this.props.ingredients,
-            price: this.props.price,
-        }
-        
         this.setState({
             loading: true,
         })
+        
+        const formData = {};
+        
+        for(let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+        }
 
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            orderData: formData
+        }
+        
         axios.post('/orders.json', order)
             .then(response => {
                 this.props.history.push('/');
@@ -66,6 +90,8 @@ class ContactData extends Component {
         }
 
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
 
         this.setState({
@@ -89,11 +115,14 @@ class ContactData extends Component {
                 elementType={element.config.elementType} 
                 elementConfig={element.config.elementConfig} 
                 value={element.config.value}
+                invalid={!element.config.valid}
+                shouldValidate={element.config.validation}
+                touched={element.config.touched}
                 changed={(event) => this.inputChangedHandler(event, element.id)}/>
         })
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {inputElements}
                 <Button type="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
